d10ea3c81669ce6d8e56801d33c49e2eaabddf0b
DEEPAK MANDAPALLI <152033387+MDeepak23@users.noreply.github.com>
2025-05-14 14:19:31 +0530

    Update StatelessWidget.dart

--- states_classi/StatelessWidget.dart
+++ states_classi/StatelessWidget.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(MyApp());
+  //void main() => runApp(MyApp());
 }
 class MyApp extends StatelessWidget{
   Widget build(BuildContext context){
